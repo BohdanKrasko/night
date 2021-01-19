776d744a59649a493301de5faaebeaebe014b51b
Bohdan Krasko <krasko200006@gmail.com>
2021-01-18 23:04:38 -0800

    output

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,22 +4,27 @@ output "public_ip" {
 
 output "db_endpoint" {
   value = module.rds.endpoint
+  sensitive = true
 }
 
 output "db_name" {
   value = module.rds.name
+  sensitive = true
 }
 
 output "db_username" {
   value = module.rds.username
+  sensitive = true
 }
 
 output "db_password" {
   value = module.rds.password
+  sensitive = true
 }
 
 output "db_port" {
   value = module.rds.port
+  sensitive = true
 }
 
 
